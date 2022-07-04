1d2bdb9dea75f82ed1c1c28ea962aa8c0837a522
italoDutr4 <italoshanti@gmail.com>
2022-07-03 22:33:53 -0300

    projeto base

--- src/main/java/inverterStrings.java
+++ src/main/java/inverterStrings.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class inverterStrings {
+
+    public static void main(String [] args) {
+        System.out.println("Insira uma palavra:");
+        Scanner leitor = new Scanner(System.in);
+        String palavra = leitor.next();
+        String novaPalavra = "";
+
+        for(int i = palavra.length() - 1; i >= 0; i--){
+            novaPalavra = novaPalavra + Character.toString(palavra.charAt(i));
+        }
+        System.out.println(novaPalavra);
+    }
 }
